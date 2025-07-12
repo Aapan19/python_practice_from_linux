ba83dcea0072a4e40640974c1055c2a1ddb47710
Aapan dxt <Aapan19@gmail.com>
2025-07-12 08:23:53 +0600

    9.7 Create User Drivers and Ride sharing

--- Phitron/user.py
+++ Phitron/user.py
@@ -14,6 +14,9 @@ class Ride_Sharing:
     def add_driver(self, driver):
         self.drivers.append(driver)
 
+    def __repr__(self):
+        return f'{self.company_name} with riders: {len(self.riders)} and drivers: {len(self.drivers)}'
+
 class User(ABC):
     def __init__(self, name, email, nid):
         self.name = name
@@ -45,13 +48,17 @@ class Rider(User):
     def update_location(self, current_location):
         self.current_location = current_location
 
-    def request_ride(self, destination):
+    def request_ride(self, ride_sharing, destination):
         if not self.current_ride:
             # to do --> set ride properly  # Done
             # to do --> set current ride via ride match  # Done
             ride_request = Ride_request(self, destination)
-            ride_matcher = Ride_matching()
-            self.current_ride = ride_matcher.find_driver(ride_request)
+            ride_matcher = Ride_matching(ride_sharing.drivers)
+            ride = ride_matcher.find_driver(ride_request)
+            self.current_ride = ride
+
+    def show_current_ride(self):
+        print(self.current_ride)
 
 class Driver(User):
     def __init__(self, name, email, nid, current_location):
@@ -86,14 +93,17 @@ class Ride:
         self.rider.wallet -= self.estimated_fare
         self.driver.wallet += self.estimated_fare
 
+    def __repr__(self):
+        return f'Ride details. Started: {self.start_location} to {self.end_location}'
+        
 class Ride_request:
     def __init__(self, rider, end_location):
         self.rider = rider
         self.end_location = end_location
 
 class Ride_matching:
-    def __init__(self):
-        self.aavailable_drivers = []
+    def __init__(self, drivers):
+        self.aavailable_drivers = drivers
 
     def find_driver(self, ride_request):
         if len(self.aavailable_drivers) > 0:
@@ -134,3 +144,15 @@ class Bike(Vehicle):
 
     def start_drive(self):
         self.status = 'Unavailable'
+
+
+# Check the class integration
+
+niye_jao = Ride_Sharing('Niye Jao')
+sakib = Rider('Sakib Khan', 'sakib.khan@gmail.com', 1234, 'mohakhali', 1200)
+niye_jao.add_rider(sakib)
+kala_pakhi = Driver('Kala pakhi', 'kala@gmail.com', 45698, 'gulshan 1')
+niye_jao.add_driver(kala_pakhi)
+print(niye_jao)
+sakib.request_ride(niye_jao, 'uttara')
+sakib.show_current_ride()
