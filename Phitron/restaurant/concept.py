623751133f81f71963e3ee6f94e0748ee8f16271
Aapan dxt <Aapan19@gmail.com>
2025-07-12 15:37:51 +0600

    Overview of the Restaurant OOP Project

---
+++ Phitron/restaurant/concept.py
@@ -0,0 +1,31 @@
+""" Concept -->
+class none:
+    1. inheritance
+    2. encapsulation
+    3. polymorphism
+    4. abstraction 
+    ------------------
+    use cases diagram
+    classes 
+    1. User: base class (ABC)
+        1.1 Customer(User)
+        1.2 Employee
+            1.2.1 Chef(Employee)
+            1.2.2 Server(Employee)
+            1.2.3 Manager(Employee)
+            1.2.4 Cleane(Employee)
+        1.3 Supplier(User)
+        1.4 Marketer(User)
+
+    2. Food_Item(ABC)
+        2.1 Pizza(Food_Item)
+        2.2 Burger(Food_Item)
+        2.3 Drinks(Food_Item)
+        2.4 Juice(Food_Item)
+        2.5 Salad(Food_Item)
+
+    3. Order
+    4. Restaurant
+
+"""
+
